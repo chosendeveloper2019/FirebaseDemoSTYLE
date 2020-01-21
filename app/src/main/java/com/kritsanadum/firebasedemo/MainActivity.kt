163121d627fc3c9ef7c87a07deb41ac59b8cc19b
kritsanarongkaew <chosendeveloper@gmail.com>
2020-01-21 15:25:30 +0700

    develop at 21-01-63

--- app/src/main/java/com/kritsanadum/firebasedemo/MainActivity.kt
+++ app/src/main/java/com/kritsanadum/firebasedemo/MainActivity.kt
@@ -3,68 +3,102 @@ package com.kritsanadum.firebasedemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-
-    private lateinit var database: DatabaseReference
-
-
+      var database = FirebaseDatabase.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //inint
+        var arrData = ArrayList<Users>()
+        var arrContractID = ArrayList<String>()
 
+        var myRef = database.getReference("/tb_user")
 
-
-        database = FirebaseDatabase.getInstance().reference
-
-
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Get Post object and use the values to update the UI
-
-                val posts = ArrayList<Users>()
-                for (snapshot in dataSnapshot.children) {
-                    val users = snapshot.getValue(Users::class.java)
-                    posts.add(users!!)
-                }
-
-                Log.e("Result"," MSG "+ posts.size)
-
-
-
-                // ...
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.w("Error", "loadPost:onCancelled", databaseError.toException())
-                // ...
-            }
-        }
-        database.addValueEventListener(postListener)
-
-
-
+        //get & push data
         btn_send.setOnClickListener {
 
-            var getName = et_input.text.toString()
+            var userObj  = Users(et_username.text.toString() , et_email.text.toString())
 
-            var users =  Users("Kritsana "+getName,"email1@email.com")
+            myRef.push().setValue(userObj)
 
-
-            database.child("tb_user").push().setValue(users)
 
         }
 
+        myRef.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+
+                //clear data
+                arrData = ArrayList<Users>()
+                arrContractID = ArrayList<String>()
+
+                var arrShow = ArrayList<String>()
+
+                var mAdapter = ArrayAdapter(this@MainActivity,android.R.layout.simple_list_item_1,arrShow)
+                var value = datasnapshot!!.children
+                value.forEach {
+
+                    arrContractID.add(it.key.toString())
+
+                    arrData.add(Users(it.child("username").value.toString() ,
+                        it.child("email").value.toString()) )
 
 
+                    arrShow.add(it.child("username").value.toString())
+                    Log.e("DATA",""+it.child("email").value)
+
+                }
+
+                mAdapter = ArrayAdapter(this@MainActivity,android.R.layout.simple_list_item_1,arrShow)
+                lv_result.adapter = mAdapter
+
+            }
+
+        })
+
+        lv_result.setOnItemClickListener { parent, view, position, id ->
+
+            var builder = AlertDialog.Builder(this@MainActivity)
+
+            var et_edit = EditText(this)
+
+            et_edit.setText(arrData.get(position).username)
+
+            builder.setView(et_edit)
+
+            builder.setTitle("แก้ไขรายการ")
+
+
+            builder.setNeutralButton("แก้ไข"){dialog, which ->
+
+                var userObj = Users(et_edit.text.toString(),arrData.get(position).email)
+                myRef.child(arrContractID.get(position)).setValue(userObj)
+
+            }
+
+            builder.setPositiveButton("ลบ"){dialog, which ->
+
+                myRef.child(arrContractID.get(position)).removeValue()
+            }
+
+            var dialog = builder.create()
+            dialog.show()
+        }
 
     }
 }
